729fbd57ed199c359e3bbb55b0bbb7412437bc74
Guilherme Fernandes de Freitas <107778711+Martyzaum@users.noreply.github.com>
2026-08-05 09:03:26 +0000

    feat(balance): process transaction events

---
+++ src/main/kotlin/br/com/itau/challenge/balance/application/ProcessTransactionEventService.kt
@@ -0,0 +1,26 @@
+package br.com.itau.challenge.balance.application
+
+import br.com.itau.challenge.balance.domain.model.AccountBalance
+import br.com.itau.challenge.balance.domain.model.TransactionEvent
+import br.com.itau.challenge.balance.port.input.ProcessTransactionEventUseCase
+import br.com.itau.challenge.balance.port.output.AccountBalanceRepository
+
+class ProcessTransactionEventService(
+    private val accountBalanceRepository: AccountBalanceRepository,
+) : ProcessTransactionEventUseCase {
+
+    override fun processTransactionEvent(event: TransactionEvent): Boolean {
+        if (!event.isEligibleForBalanceUpdate()) {
+            return false
+        }
+
+        return accountBalanceRepository.saveIfNewer(
+            AccountBalance(
+                id = event.accountId,
+                owner = event.accountOwner,
+                balance = event.balance,
+                updatedAtMicros = event.timestampMicros,
+            ),
+        )
+    }
+}
